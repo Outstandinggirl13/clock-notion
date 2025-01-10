8273e5e8585aa0612420ecd2f68ca296b88105d9
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2025-01-10 22:33:43 +0200

    Hours format issue is resolved.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const clockHours = document.querySelector(".clock-hours");
 const clockMinutes = document.querySelector(".clock-minutes");
 const clocMeridiem = document.querySelector(".clock-meridiem");
@@ -14,21 +12,26 @@ function startTime () {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-    let apm = h > 11 ? "PM" : "AM";
+    h = checkMeridiem(h);
     h = checkTime (h);
     m = checkTime(m);
     s = checkTime(s);
-    clockHours.innerText = h;
+    clockHours.innerText = h[0];
     clockMinutes.innerText = m;
-    clocMeridiem.innerText = apm;
+    clocMeridiem.innerText = h[1];
     clockSeconds.innerText = s;
     clockDay.innerText = wd;
     setTimeout(startTime, 1000);
 }
 
-function checkTime(i) {
-    if (i < 10) {i = `0${i}`};  // add zero in front of numbers < 10
-    return i;
+function checkTime(x) {
+    if (x < 10) {x = `0${x}`};  // add zero in front of numbers < 10
+    return x;
+}
+
+function checkMeridiem(x, y = "AM") {
+    if (x > 11) {x = x-12; y = "PM"};
+    return [x, y];
 }
 
 window.addEventListener('load', startTime());
