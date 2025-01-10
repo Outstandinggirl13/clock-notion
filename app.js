6e6697c792356bd4ef4a6273ebd836e7a794fddd
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2025-01-11 00:35:34 +0200

    Clean, simple and stable version 1.0.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const clockHours = document.querySelector(".clock-hours");
 const clockMinutes = document.querySelector(".clock-minutes");
 const clocMeridiem = document.querySelector(".clock-meridiem");
-const clockSeconds = document.querySelector(".clock-seconds");
 const clockDay = document.querySelector(".clock-day");
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -11,21 +10,18 @@ function startClock () {
     let wd = weekDays[today.getDay()];
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds();
     h = checkMeridiem(h);
     clocMeridiem.innerText = h[1];
     h = checkTime(h[0]);
     m = checkTime(m);
-    s = checkTime(s);
     clockHours.innerText = h;
     clockMinutes.innerText = m;
-    clockSeconds.innerText = s;
     clockDay.innerText = wd;
     setTimeout(startClock, 1000);
 }
 
 function checkTime(x) {
-    if (x < 10) {x = `0${x}`};  // add zero in front of numbers < 10
+    if (x < 10) {x = `0${x}`};
     return x;
 }
 
--- customization.js
+++
@@ -1,6 +0,0 @@
-const btnCustomize = document.querySelector(".set-colors")
-
-document.addEventListener("click", function() {
-    btnCustomize.style.display = 'inline-block';
-});
-
