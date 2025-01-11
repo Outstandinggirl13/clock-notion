cf207991b26c51c36b055f0fd8dd792dfb474b81
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2025-01-11 14:08:03 +0200

    Improved performance: the clock now refreshes every minute. The bug with 0 PM has been fixed.

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const clockDay = document.querySelector(".clock-day");
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function startClock () {
-    const today = new Date();
-    let wd = weekDays[today.getDay()];
-    let h = today.getHours();
-    let m = today.getMinutes();
+const initialDate = new Date();
+timepiece(initialDate); // set the current time in the HTML elements of the digital clock
+const delay = (60-(initialDate).getSeconds())*1000; // calculate the delay in milliseconds before running the startClock() function, which refreshes the time every minute
+
+function startClock () { // start the clock at the current time and refresh it every 60 seconds
+    timepiece(new Date());
+    setTimeout(startClock, 60000);
+}
+
+function timepiece(currentTime) { // Check the current day of the week, hours, minutes, and meridiem and record them in the corresponding HTML elements
+    let wd = weekDays[currentTime.getDay()];
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
     h = checkMeridiem(h);
     clocMeridiem.innerText = h[1];
     h = checkTime(h[0]);
@@ -17,17 +25,16 @@ function startClock () {
     clockHours.innerText = h;
     clockMinutes.innerText = m;
     clockDay.innerText = wd;
-    setTimeout(startClock, 1000);
 }
 
-function checkTime(x) {
-    if (x < 10) {x = `0${x}`};
+function checkTime(x) { // add a zero before single-digit numbers
+    if (x < 10) {x = `0${x}`}; 
     return x;
 }
 
-function checkMeridiem(x, y = "AM") {
-    if (x > 11) {x = x-12; y = "PM"};
+function checkMeridiem(x, y = "AM") { // determine whether it is before or after noon, and return an array [hours, 'meridiem']
+    if (x > 12) {x = x-12; y = "PM"}; 
     return [x, y];
 }
 
-window.addEventListener('load', startClock());
+window.addEventListener('load', () => setTimeout(startClock, delay)); // after the page loads, wait for a delay, then run the startClock() function
